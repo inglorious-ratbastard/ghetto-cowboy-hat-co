e949961bd4d2fced1b09339848cc78a0dfc152ef
Javier Yzaguirre <47618267+inglorious-ratbastard@users.noreply.github.com>
2025-04-13 18:55:28 -0500

    Update main.js

--- main.js
+++ main.js
@@ -10,3 +10,17 @@ setInterval(() => {
     bannerText.classList.add("fade-in"); 
 }, 10000);
 
+
+
+document.querySelectorAll('.block').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+        // Optional: update the URL without the hash
+        history.replaceState(null, null, ' ');
+      }
+    });
+  })
