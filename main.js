962102109ddd1fd4cb27b148314c41bd599407f7
Javier Yzaguirre <47618267+inglorious-ratbastard@users.noreply.github.com>
2025-04-13 18:17:29 -0500

    Update main.js

--- main.js
+++ main.js
@@ -10,43 +10,6 @@ setInterval(() => {
     bannerText.classList.add("fade-in"); 
 }, 10000);
 
-
-export class Utils {
-    constructor(selector) {
-        this.elements = Utils.getSelector(selector);
-        this.element = this.get(0);
-        return this;
-    }
-
-    static getSelector(selector, context) {
-        if (selector && typeof selector !== 'string') {
-            if (selector.length !== undefined) {
-                return selector;
-            }
-            return [selector];
-        }
-        context = context || document;
-
-        // For performance reasons, use getElementById
-        // eslint-disable-next-line no-control-regex
-        const idRegex = /^#(?:[\w-]|\\.|[^\x00-\xa0])*$/;
-        if (idRegex.test(selector)) {
-            const el = document.getElementById(selector.substring(1));
-            return el ? [el] : [];
-        }
-        return [].slice.call(context.querySelectorAll(selector) || []);
-    }
-    get(index) {
-        if (index !== undefined) {
-            return this.elements[index];
-        }
-        return this.elements;
-    }
-}
-
-Utils.eventListeners = {};
-
-export default function $utils(selector) {
-    return new Utils(selector);
-}
-
+setTimeout(()=>{
+  history.replaceState('', document.title, window.location.origin + window.location.pathname + window.location.search);
+}, 5);
