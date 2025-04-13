0acaf5dc6351fccbfa02915552a441564472a8f0
Javier Yzaguirre <47618267+inglorious-ratbastard@users.noreply.github.com>
2025-04-13 18:23:45 -0500

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,3 @@ setInterval(() => {
     bannerText.textContent = texts[index];
     bannerText.classList.add("fade-in"); 
 }, 10000);
-
-setTimeout(()=>{
-  history.replaceState('', document.title, window.location.origin + window.location.pathname + window.location.search);
-}, 5);
